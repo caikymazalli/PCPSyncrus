c622c7b4ac12b6bb2998566c095f782702201f09
caikymazalli <caiky@syncrus.com.br>
2026-02-28 13:20:08 -0300

    Refactor instrucoes API endpoint to Hono
    Refactored API endpoint to use Hono framework and retrieve instructions based on tenant context.

--- src/routes/instrucoes.ts
+++ src/routes/instrucoes.ts
@@ -1,4 +1,15 @@
-import { tenant } from '../models';
+import { Hono } from 'hono'
+import { getCtxTenant, getCtxUserId } from '../sessionHelper'
+
+const app = new Hono()
+
+app.get('/api/instrucoes', (c) => {
+  const tenant = getCtxTenant(c)
+  const instructions = tenant.instructions || []
+  return c.json(instructions)
+})
+
+export default appimport { tenant } from '../models';
 
 // ... other code ...
 
