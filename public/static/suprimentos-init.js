fb87b11b2b4c168c87008ea5b5e751c654bf9f72
caikymazalli <caiky@syncrus.com.br>
2026-03-02 08:21:34 -0300

    Add reenviarCotacao function to suprimentos-init.js

--- public/static/suprimentos-init.js
+++ public/static/suprimentos-init.js
@@ -1,1 +1,6 @@
-console.log('Suprimentos initialized'); // Example content
+function reenviarCotacao() {
+    // Function logic goes here
+    console.log('Cotação reenviada.');
+}
+
+// Current Date and Time: 2026-03-02 11:21:19
